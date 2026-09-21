57f9d1c122299258cadee56fed43faa029c384a4
AlfredoNavarroDev <alfredont1088@gmail.com>
2026-09-21 18:28:20 -0500

    chore: scaffold backend with TypeORM, config, and moved entities/migrations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Business } from './entities/business.entity';
+import { Product } from './entities/product.entity';
+import { Purchase } from './entities/purchase.entity';
+import { Sale } from './entities/sale.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.getOrThrow<string>('DATABASE_URL'),
+        entities: [User, Business, Product, Purchase, Sale],
+        synchronize: false,
+        ssl: config.get('DATABASE_SSL') === 'true' ? { rejectUnauthorized: false } : false,
+        logging: config.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
